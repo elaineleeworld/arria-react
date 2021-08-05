9f24eec299b95dcd8b9b43ab6a84fb73a2ef0bef
Lee, Elaine <elee10@amgen.com>
2021-08-04 20:20:32 -0700

    add a return

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  useEffect(() => {
+    fetch('https://amgen.prod.arria.com:443/alite_content_generation_webapp/text/k0XDl0BKj33', {
+          'method': "GET",
+          'headers': {
+          'Content-Type': 'application/json;charset=UTF-8',
+          'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJibTBrWHBQZVl3U2tfc2hIYVljN1d1ZFoiLCJpYXQiOjE2Mjc5MTY5NzgsImV4cCI6MTc4NTU5Njk3OCwiaXNzIjoiQUxpdGUiLCJzdWIiOiJCSDlyYnBoRW95eXIiLCJBTGl0ZS5wZXJtIjpbInByczp4OmswWERsMEJLajMzIl0sIkFMaXRlLnR0IjoidV9hIn0.2-9Gr7uTmEQqnM30t52kSjO2awDMgym13CpNGCw0M-7rWZCFTnrjINx6pqrsiwd7suW01j3wLmuRP7mvKEDyAQ'
+          }
+    })
+    .then(response => response.json())
+    .then(data => console.log('DATA:', data))
+    .catch(error => console.log('ERROR:', error))
+
+  },[])
+
+  
+    return (
+      <div>TEST</div>
+    )
+  
 }
 
 export default App;
