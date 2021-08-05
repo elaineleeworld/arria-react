a4ebd0d9e31258f327a0ed1a15dc0e0ef5fef936
Lee, Elaine <elee10@amgen.com>
2021-08-04 20:23:41 -0700

    add no-cors

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   useEffect(() => {
     fetch('https://amgen.prod.arria.com:443/alite_content_generation_webapp/text/k0XDl0BKj33', {
           'method': "GET",
+          "mode": "no-cors",
           'headers': {
           'Content-Type': 'application/json;charset=UTF-8',
           'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJibTBrWHBQZVl3U2tfc2hIYVljN1d1ZFoiLCJpYXQiOjE2Mjc5MTY5NzgsImV4cCI6MTc4NTU5Njk3OCwiaXNzIjoiQUxpdGUiLCJzdWIiOiJCSDlyYnBoRW95eXIiLCJBTGl0ZS5wZXJtIjpbInByczp4OmswWERsMEJLajMzIl0sIkFMaXRlLnR0IjoidV9hIn0.2-9Gr7uTmEQqnM30t52kSjO2awDMgym13CpNGCw0M-7rWZCFTnrjINx6pqrsiwd7suW01j3wLmuRP7mvKEDyAQ'
