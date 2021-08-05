4b6d3e8e29c15ee02375306dcbef921d1d35996a
Lee, Elaine <elee10@amgen.com>
2021-08-04 21:14:40 -0700

    testing fetch without no-cors

--- src/App.js
+++ src/App.js
@@ -5,22 +5,35 @@ import { myConfig } from './config.js'
 
 
 function App() {
-  const apiKey = myConfig.SECRET_API_KEY;
   
-  useEffect(() => {
-    fetch('https://amgen.prod.arria.com:443/alite_content_generation_webapp/text/k0XDl0BKj33', {
-          'method': "GET",
-          "mode": "no-cors",
-          'headers': {
-          'Content-Type': 'application/json;charset=UTF-8',
-          'Authorization': 'Bearer apiKey'
-          }
+  // const obj = {
+  //         'method': "GET",
+  //         "mode": "no-cors",
+  //         'headers': {
+  //         'Content-Type': 'application/json;charset=UTF-8',
+  //         'Authorization': 'Bearer myConfig.SECRET_API_KEY'
+  //         },
+  // }
+  // useEffect(() => {
+    fetch('https://amgen.prod.arria.com:443/alite_content_generation_webapp/text/k0XDl0BKj33', 
+    {
+      // 'method': "GET",
+      // "mode": "no-cors",
+      'headers': {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Authorization': 'Bearer myConfig.SECRET_API_KEY'
+      },
     })
     .then(response => response.json())
     .then(data => console.log('DATA:', data))
     .catch(error => console.log('ERROR:', error))
 
-  },[])
+    // fetch('https://api.publicapis.org/entries')
+    // .then(response => response.json())
+    // .then(data => console.log('DATA:', data))
+    // .catch(error => console.log('ERROR:', error))
+
+  // },[])
 
   
     return (
